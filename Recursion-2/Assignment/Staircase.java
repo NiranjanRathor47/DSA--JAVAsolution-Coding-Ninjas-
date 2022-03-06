774069152f682a45b99d51260a8a0a3636e7f8e1
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2022-03-06 18:05:41 +0530

    Add files via upload

---
+++ Recursion-2/Assignment/Staircase.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Staircase {
+	public static int staircase(int n){
+		 if(n<0)
+	            return 0;
+	        if(n==0)
+	            return 1;
+	        int op1=staircase(n-1);
+	        int op2=staircase(n-2);
+	        int op3=staircase(n-3);
+	        return op1+op2+op3;
+	}
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt(); 
+		int res = staircase(n);
+		System.out.print(res);
+	}
+
+}
