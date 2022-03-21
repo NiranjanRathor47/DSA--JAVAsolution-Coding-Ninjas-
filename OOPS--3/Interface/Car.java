3830c0ac541144b6a5a6387a19fbc7c915570e77
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2022-03-22 00:58:30 +0530

    Add files via upload

---
+++ OOPS--3/Interface/Car.java
@@ -0,0 +1,23 @@
+package Interfaces;
+
+public class Car extends Vehicle implements CarInterface{
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public int getMaxSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public String getCompany() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
