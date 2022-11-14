471696a199fe01bf2b12f622d2aa6535debbd718
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2022-11-15 01:50:54 +0530

    Add files via upload

---
+++ Binary Tree/Assignment/CheckBalancedBinaryTree.java
@@ -0,0 +1,68 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class CheckBalancedBinaryTree {
+
+	public static int height(BinaryTreeNode<Integer> root) {
+		if(root==null) {
+			return 0 ;
+		}
+		return 1 + Math.max(height(root.left),height(root.right));
+	}
+	public static boolean checkBalanced(BinaryTreeNode<Integer> root) {
+		if(root==null) {
+			return true;
+		}
+		int lh = height(root.left);
+		int rh = height(root.right);
+		
+		if(Math.abs(lh-rh)<=1 && checkBalanced(root.left) && checkBalanced(root.right)) {
+			return true ;
+		}
+		return false ;
+	}
+	
+	public static BinaryTreeNode<Integer> takeInputLevelWise() {
+		Scanner s = new Scanner(System.in);
+		QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+		System.out.println("Enter root data");
+		int rootData = s.nextInt();
+		if (rootData == -1) {
+			return null;
+		}
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> front;
+			try {
+				front = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			System.out.println("Enter left child of " + front.data);
+			int leftChild = s.nextInt();
+			if (leftChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(leftChild);
+				pendingNodes.enqueue(child);
+				front.left = child;
+			}
+			
+			System.out.println("Enter right child of " + front.data);
+			int rightChild = s.nextInt();
+			if (rightChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(rightChild);
+				pendingNodes.enqueue(child);
+				front.right = child;
+			}
+		}
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		boolean res =  checkBalanced(root);
+		System.out.print(res);
+	}
+}
---
+++ Binary Tree/Assignment/LevelOrderTraversal.java
@@ -0,0 +1,77 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class LevelOrderTraversal {
+
+	public static void printLevelWise(BinaryTreeNode<Integer> root) {
+		QueueUsingLL<BinaryTreeNode<Integer>>  primary = new QueueUsingLL<>();
+		QueueUsingLL<BinaryTreeNode<Integer>>  secondary = new QueueUsingLL<>();
+
+		primary.enqueue(root);
+
+		while(!primary.isEmpty()){
+			BinaryTreeNode<Integer> current=null;
+			try {
+				current = primary.dequeue();
+			} catch (QueueEmptyException e) {
+				return ;
+			}
+			System.out.print(current.data + " ");
+			if(current.left != null){
+				secondary.enqueue(current.left);
+			}
+			if(current.right != null){
+				secondary.enqueue(current.right);
+			}
+			if(primary.isEmpty()){
+				QueueUsingLL<BinaryTreeNode<Integer>>  temp = secondary;
+				secondary = primary;
+				primary = temp;
+				System.out.println();
+			}
+		}
+	}
+
+	public static BinaryTreeNode<Integer> takeInputLevelWise() {
+		Scanner s = new Scanner(System.in);
+		QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+		System.out.println("Enter root data");
+		int rootData = s.nextInt();
+		if (rootData == -1) {
+			return null;
+		}
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> front;
+			try {
+				front = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			System.out.println("Enter left child of " + front.data);
+			int leftChild = s.nextInt();
+			if (leftChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(leftChild);
+				pendingNodes.enqueue(child);
+				front.left = child;
+			}
+			
+			System.out.println("Enter right child of " + front.data);
+			int rightChild = s.nextInt();
+			if (rightChild != -1) {
+				BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(rightChild);
+				pendingNodes.enqueue(child);
+				front.right = child;
+			}
+		}
+		return root;
+	}
+    
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		printLevelWise(root);
+	}
+}
